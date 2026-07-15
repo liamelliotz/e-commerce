e10dd5f62aa48d0d0855075d56b464ac678b2705
Liam Elliot <liamelliotllk@gmail.com>
2026-07-15 18:55:35 -0300

    feat: integra componente de lista de produtos

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
 //import { RouterOutlet } from '@angular/router';//remove a importação de RouterOutlet, pois não é necessário para este componente
-import { Produto } from './components/produto/produto'; //importando a classe Produto do arquivo produto.ts para ser usado no componente app.ts
+//import { Produto } from './components/produto/produto'; //importando a classe Produto do arquivo produto.ts para ser usado no componente app.ts
+import { ListaProdutos } from './components/lista-produtos/lista-produtos';
 @Component({
   selector: 'app-root',
-  imports: [Produto],
+  imports: [ListaProdutos],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
