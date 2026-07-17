0020b665f3a006764ead28def76e42a8ee0a387a
Liam Elliot <liamelliotllk@gmail.com>
2026-07-17 19:34:26 -0300

    feat: implementa substituição da lista de produtos com set

--- src/app/features/produtos/lista-produtos/lista-produtos.ts
+++ src/app/features/produtos/lista-produtos/lista-produtos.ts
@@ -11,29 +11,37 @@ import { PrecoFormatadoPipe } from '../../../shared/pipes/preco-formatado-pipe'
   styleUrl: './lista-produtos.css',
 })
 export class ListaProdutos {
-// lista com dados - Array
+//!lista com dados - Array
   produtos = signal([
     {nome: 'Fone Gamer', preco:345.99},
     {nome: 'Teclado Gamer Arceus', preco: 456.99},
     {nome: 'RTX 5090', preco: 25999.99}
   ]);
-// função para exibir produtos selecionados pelo usuário no console 
+//!função para exibir produtos selecionados pelo usuário no console 
   exibirProduto(nome: string){
     console.log('Produto Selecionado: ', nome);
   }
-// funcão que adiciona produtos usando metodo update()
+//!funcão que adiciona produtos usando metodo update()
   adicionarProduto(){
     this.produtos.update(listaAtual => [
       ...listaAtual,
       {nome:'Playstation 5', preco:3000},
     ]);
   }
-//função que contabiliza a quantidade de produtos na lista com o metodo computed()
+//!função que contabiliza a quantidade de produtos na lista com o metodo computed()
   totalProdutos = computed(() => this.produtos().length);
-// funcao que calcula o valor total do produtos usando o metodo computed()
+//!funcao que calcula o valor total do produtos usando o metodo computed()
   valorTotal = computed(() =>
   {return this.produtos().reduce((total, item) =>
-  total + item.preco, 0
-  )}
-);
+  total + item.preco, 0)});
+  //!função para substituir a lista atual usando o metodo set()
+substituirProdutos(){
+  this.produtos.set([
+    { nome:'Teclado', preco:50 },
+    { nome:'Mouse', preco:15 },
+    { nome:'Monitor', preco:500 },
+    { nome:'Desktop', preco:1500 },
+    { nome:'Headset', preco:30 },
+  ]);
+ }
 }
