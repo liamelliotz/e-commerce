5d25481badce9b446157c84344024420c55216a1
Liam Elliot <liamelliotllk@gmail.com>
2026-07-17 18:56:55 -0300

    feat: adicionar atualização e cálculos com Signals

--- src/app/features/produtos/lista-produtos/lista-produtos.ts
+++ src/app/features/produtos/lista-produtos/lista-produtos.ts
@@ -1,26 +1,39 @@
 import { Component } from '@angular/core';
 import { Produto } from '../produto/produto';
 import {signal} from '@angular/core';
+import { computed } from '@angular/core';
+import { PrecoFormatadoPipe } from '../../../shared/pipes/preco-formatado-pipe'
 
 @Component({
   selector: 'app-lista-produtos',
-  imports: [Produto],
+  imports: [Produto, PrecoFormatadoPipe],
   templateUrl: './lista-produtos.html',
   styleUrl: './lista-produtos.css',
 })
 export class ListaProdutos {
+// lista com dados - Array
   produtos = signal([
     {nome: 'Fone Gamer', preco:345.99},
     {nome: 'Teclado Gamer Arceus', preco: 456.99},
     {nome: 'RTX 5090', preco: 25999.99}
   ]);
+// função para exibir produtos selecionados pelo usuário no console 
   exibirProduto(nome: string){
     console.log('Produto Selecionado: ', nome);
   }
+// funcão que adiciona produtos usando metodo update()
   adicionarProduto(){
     this.produtos.update(listaAtual => [
       ...listaAtual,
       {nome:'Playstation 5', preco:3000},
     ]);
   }
+//função que contabiliza a quantidade de produtos na lista com o metodo computed()
+  totalProdutos = computed(() => this.produtos().length);
+// funcao que calcula o valor total do produtos usando o metodo computed()
+  valorTotal = computed(() =>
+  {return this.produtos().reduce((total, item) =>
+  total + item.preco, 0
+  )}
+);
 }
