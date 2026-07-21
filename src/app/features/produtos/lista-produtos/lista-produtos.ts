5e25b27256aaf713fe9d0eb5a539fa72526f1ff5
Liam Elliot <liamelliotllk@gmail.com>
2026-07-21 19:30:48 -0300

    feat: exibe itens, quantidade e valor total do carrinho

--- src/app/features/produtos/lista-produtos/lista-produtos.ts
+++ src/app/features/produtos/lista-produtos/lista-produtos.ts
@@ -63,5 +63,16 @@ constructor(){
 }
 //!metodo para criar um estado de seleção com signal string / null
 produtoSelecionado = signal <string | null>(null);
-
+//!metodo para criar um estado para carrinho com signal
+carrinho = signal <{nome: string; preco: number}[]>([]);
+  adicionarAoCarrinho(produto:{nome: string; preco: number}){
+      this.carrinho.update(listaAtual => [...listaAtual, produto]
+        );
+          }
+//!metodo para calcular a quantidade total de itens no carrinho
+  quantidadeCarrinho = computed(() => this.carrinho().length);
+//!metodo que calcula o valor total dos itens do carrinho
+  totalCarrinho = computed(() => {
+  return this.carrinho().reduce((total, item) =>
+  total + item.preco, 0)});
 }
