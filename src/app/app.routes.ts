5f2d835514aec69a7954235e07e1dc225b3a2780
Liam Elliot <liamelliotllk@gmail.com>
2026-07-21 19:26:28 -0300

    feat: configura rotas para produtos e carrinho

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
-
-export const routes: Routes = [];
+import { ListaProdutos } from './features/produtos/lista-produtos/lista-produtos';
+import { Carrinho } from './features/carrinho/carrinho/carrinho';
+export const routes: Routes = [
+    {
+        path:'',
+        component: ListaProdutos,
+    },
+    {
+        path:'carrinho',
+        component: Carrinho,
+    },
+];
