8100271eb9fecd2aa162d413db447edf56ab7670
Liam Elliot <liamelliotllk@gmail.com>
2026-07-21 19:49:31 -0300

    style: formata arquivos com Prettier

--- src/app/features/produtos/lista-produtos/lista-produtos.ts
+++ src/app/features/produtos/lista-produtos/lista-produtos.ts
@@ -1,78 +1,74 @@
 import { Component } from '@angular/core';
 import { Produto } from '../produto/produto';
-import {signal} from '@angular/core';
+import { signal } from '@angular/core';
 import { computed } from '@angular/core';
-import { PrecoFormatadoPipe } from '../../../shared/pipes/preco-formatado-pipe'
+import { PrecoFormatadoPipe } from '../../../shared/pipes/preco-formatado-pipe';
 import { effect } from '@angular/core';
 import { UpperCasePipe } from '@angular/common';
 
 @Component({
   selector: 'app-lista-produtos',
-  imports: [Produto,PrecoFormatadoPipe,UpperCasePipe],
+  imports: [Produto, PrecoFormatadoPipe, UpperCasePipe],
   templateUrl: './lista-produtos.html',
   styleUrl: './lista-produtos.css',
 })
 export class ListaProdutos {
-//!lista com dados - Array
+  //!lista com dados - Array
   produtos = signal([
-    {nome: 'Fone Gamer', preco:345.99},
-    {nome: 'Teclado Gamer Arceus', preco: 456.99},
-    {nome: 'RTX 5090', preco: 25999.99}
+    { nome: 'Fone Gamer', preco: 345.99 },
+    { nome: 'Teclado Gamer Arceus', preco: 456.99 },
+    { nome: 'RTX 5090', preco: 25999.99 },
   ]);
-//!função para exibir produtos selecionados pelo usuário no console 
-  exibirProduto(nome: string){
+  //!função para exibir produtos selecionados pelo usuário no console
+  exibirProduto(nome: string) {
     console.log('Produto Selecionado: ', nome);
     this.produtoSelecionado.set(nome);
   }
-//!funcão que adiciona produtos usando metodo update()
-  adicionarProduto(){
-    this.produtos.update(listaAtual => [
-      ...listaAtual,
-      {nome:'Playstation 5', preco:3000},
+  //!funcão que adiciona produtos usando metodo update()
+  adicionarProduto() {
+    this.produtos.update((listaAtual) => [...listaAtual, { nome: 'Playstation 5', preco: 3000 }]);
+  }
+  //!função que contabiliza a quantidade de produtos na lista com o metodo computed()
+  totalProdutos = computed(() => this.produtos().length);
+  //!funcao que calcula o valor total do produtos usando o metodo computed()
+  valorTotal = computed(() => {
+    return this.produtos().reduce((total, item) => total + item.preco, 0);
+  });
+  //!função para substituir a lista atual usando o metodo set()
+  substituirProdutos() {
+    this.produtos.set([
+      { nome: 'Teclado', preco: 50 },
+      { nome: 'Mouse', preco: 15 },
+      { nome: 'Monitor', preco: 500 },
+      { nome: 'Desktop', preco: 1500 },
+      { nome: 'Headset', preco: 30 },
     ]);
   }
-//!função que contabiliza a quantidade de produtos na lista com o metodo computed()
-  totalProdutos = computed(() => this.produtos().length);
-//!funcao que calcula o valor total do produtos usando o metodo computed()
-  valorTotal = computed(() =>
-  {return this.produtos().reduce((total, item) =>
-  total + item.preco, 0)});
-//!função para substituir a lista atual usando o metodo set()
-substituirProdutos(){
-  this.produtos.set([
-    { nome:'Teclado', preco:50 },
-    { nome:'Mouse', preco:15 },
-    { nome:'Monitor', preco:500 },
-    { nome:'Desktop', preco:1500 },
-    { nome:'Headset', preco:30 },
-  ]);
- }
-//!metodo para monitorar as alterações em tempo real usando effect()
-constructor(){
-  effect(() =>{
-    console.log('Lista de Produtos Alterados: ', this.produtos());
-  });
-  effect(() =>{
-    console.log('Valor Total Atualizado: ', this.valorTotal());
-  });
-  effect(() =>{
-    if (typeof document !== 'undefined'){
-     document.title = `(${this.totalProdutos()}) - Sunrise Store`;
-    }
-  });
-}
-//!metodo para criar um estado de seleção com signal string / null
-produtoSelecionado = signal <string | null>(null);
-//!metodo para criar um estado para carrinho com signal
-carrinho = signal <{nome: string; preco: number}[]>([]);
-  adicionarAoCarrinho(produto:{nome: string; preco: number}){
-      this.carrinho.update(listaAtual => [...listaAtual, produto]
-        );
-          }
-//!metodo para calcular a quantidade total de itens no carrinho
+  //!metodo para monitorar as alterações em tempo real usando effect()
+  constructor() {
+    effect(() => {
+      console.log('Lista de Produtos Alterados: ', this.produtos());
+    });
+    effect(() => {
+      console.log('Valor Total Atualizado: ', this.valorTotal());
+    });
+    effect(() => {
+      if (typeof document !== 'undefined') {
+        document.title = `(${this.totalProdutos()}) - Sunrise Store`;
+      }
+    });
+  }
+  //!metodo para criar um estado de seleção com signal string / null
+  produtoSelecionado = signal<string | null>(null);
+  //!metodo para criar um estado para carrinho com signal
+  carrinho = signal<{ nome: string; preco: number }[]>([]);
+  adicionarAoCarrinho(produto: { nome: string; preco: number }) {
+    this.carrinho.update((listaAtual) => [...listaAtual, produto]);
+  }
+  //!metodo para calcular a quantidade total de itens no carrinho
   quantidadeCarrinho = computed(() => this.carrinho().length);
-//!metodo que calcula o valor total dos itens do carrinho
+  //!metodo que calcula o valor total dos itens do carrinho
   totalCarrinho = computed(() => {
-  return this.carrinho().reduce((total, item) =>
-  total + item.preco, 0)});
+    return this.carrinho().reduce((total, item) => total + item.preco, 0);
+  });
 }
