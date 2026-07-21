58fef5e38932bbacd090f0cd4c93b9867f68f31c
Liam Elliot <liamelliotllk@gmail.com>
2026-07-21 19:27:27 -0300

    feat: configura componente principal da aplicação

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,13 +1,14 @@
 import { Component, signal } from '@angular/core';
-//import { RouterOutlet } from '@angular/router';//remove a importação de RouterOutlet, pois não é necessário para este componente
-//import { Produto } from './components/produto/produto'; //importando a classe Produto do arquivo produto.ts para ser usado no componente app.ts
-import { ListaProdutos } from './features/produtos/lista-produtos/lista-produtos';
+import { RouterOutlet, RouterLink } from '@angular/router';
+import { UpperCasePipe } from '@angular/common';
+
 @Component({
   selector: 'app-root',
-  imports: [ListaProdutos],
+  imports: [RouterOutlet, RouterLink, UpperCasePipe],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('e-commerce');
+  nomeLoja = 'Sunrise Store';
 }
