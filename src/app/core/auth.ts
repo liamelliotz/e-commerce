1e09f9ef728627c4204e2af8134414f32493e3c3
Liam Elliot <liamelliotllk@gmail.com>
2026-07-22 21:30:29 -0300

    feat: implementa estado de autenticação com signals

--- src/app/core/auth.ts
+++ src/app/core/auth.ts
@@ -1,3 +1,10 @@
 import { signal } from '@angular/core';
 
-export const usuarioLogado = signal (false);
+export const usuarioLogado = signal(false);
+export function login() {
+  usuarioLogado.set(true);
+}
+
+export function logout() {
+  usuarioLogado.set(false);
+}
