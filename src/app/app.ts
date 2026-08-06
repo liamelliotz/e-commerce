ad37336b93e53ea55a549517d6e6b218d31bd5a7
Luana Cardoso <luanamartinscardoso02@gmail.com>
2026-08-06 18:58:51 -0300

    feat: integra header e autenticação na aplicação

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,16 +1,22 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet, RouterLink } from '@angular/router';
-import { UpperCasePipe } from '@angular/common';
+import { RouterOutlet } from '@angular/router';
 import { usuarioLogado, login, logout } from './core/auth';
+import { MatButtonModule } from '@angular/material/button';
+import { Header } from './shared/layout/header/header';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, RouterLink, UpperCasePipe], //Stand Alone = Independente
+  imports: [
+    RouterOutlet,
+    MatButtonModule,
+    Header
+  ],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
 export class App {
   protected readonly title = signal('e-commerce');
+
   nomeLoja = 'Sunrise Store';
   usuarioLogado = usuarioLogado;
   login = login;
