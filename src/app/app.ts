6e8ad13bd492605f780d25797046e44782287700
Liam Elliot <liamelliotllk@gmail.com>
2026-08-06 20:27:21 -0300

    chore: importa MatCardModule no app.ts

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,14 +1,19 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { usuarioLogado, login, logout } from './core/auth';
+
 import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+
 import { Header } from './shared/layout/header/header';
 
 @Component({
   selector: 'app-root',
+  standalone: true,
   imports: [
     RouterOutlet,
     MatButtonModule,
+    MatCardModule,
     Header
   ],
   templateUrl: './app.html',
