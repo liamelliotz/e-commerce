65fd1e15b1513df31681f1d98239f4d3ab495a27
Liam Elliot <liamelliotllk@gmail.com>
2026-08-14 18:45:01 -0300

    refactor: remover imports e configuração anterior do componente principal

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,20 +1,12 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet, RouterLink } from '@angular/router';
-import { UpperCasePipe } from '@angular/common';
-import { LowerCasePipe } from '@angular/common';
-import { usuarioLogado, login, logout } from './core/auth';
-import { MatButtonModule } from '@angular/material/button';
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
 import { Header } from './shared/layout/header/header';
+
 @Component({
-  selector: 'app-root',
-  imports: [RouterOutlet, RouterLink, UpperCasePipe, MatButtonModule, Header],
-  templateUrl: './app.html',
-  styleUrl: './app.css'
+selector: 'app-root',
+imports: [RouterOutlet, Header],
+templateUrl: './app.html',
+styleUrl: './app.css'
 })
-export class App {
-  protected readonly title = signal('e-commerce-aluno');
-  nomeLoja = 'Mercado Liso';
-  usuarioLogado = usuarioLogado;
-  login = login;
-  logout = logout;
-}
+export class App {}
